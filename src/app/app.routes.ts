4c9a210d5b67a3b4b603e01ab2ee5627aa9929b0
shahzaib0791 <shahzaib0791@gmail.com>
2024-09-06 14:02:25 +0200

    first commit

--- src/app/app.routes.ts
+++
@@ -1,8 +0,0 @@
-import { Routes } from '@angular/router';
-import { TodosComponent } from './MyComponent/todos/todos.component';
-import { AboutComponent } from './MyComponent/about/about.component';
-
-export const routes: Routes = [
-    { path:'', component: TodosComponent},
-    { path: 'about', component: AboutComponent}
-];
